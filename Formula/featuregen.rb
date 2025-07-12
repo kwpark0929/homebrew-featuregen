9dc130c9e808a4fa1e3cc50b9361186739f74d29
kwpark0929 <42733211+kwpark0929@users.noreply.github.com>
2025-07-12 14:16:01 +0900

    Update featuregen.rb

--- Formula/featuregen.rb
+++ Formula/featuregen.rb
@@ -1,9 +1,9 @@
 class Featuregen < Formula
   desc "Swift TCA Feature 자동 생성기"
   homepage "https://github.com/kwpark0929/featuregen"
-  url "https://github.com/kwpark0929/featuregen/releases/download/v1.0.2/featuregen.zip"
-  sha256 "845d341cd8dcfe8a371e3ce392214b5d2871277c6d667a75a2ca65d8be6f3801"
-  version "1.0.2"
+  url "https://github.com/kwpark0929/featuregen/releases/download/v1.0.3/featuregen.zip"
+  sha256 "507e4394702e01d620817617121b96e60478bb82f685e033002d2fa7f52c0144"
+  version "1.0.3"
 
   def install
     bin.install "featuregen"
